33ef379f7ab7dd1f04d6a8683e6ca0bb2572515d
JWatkins20 <38019485+JWatkins20@users.noreply.github.com>
2020-09-24 15:38:03 -0400

    different subreddits picked at random

--- bot.py
+++ bot.py
@@ -1,12 +1,38 @@
 import twitter
 import praw
 import time
+import threading
+import random
 from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
 import matplotlib.pyplot as plt
 
 # To filter out all of the words that don't hold much meaning
 # alone(articles, affirmatives, neutral adjectives and verbs)
 # so they don't show up in the wordcloud
+subreddits = ["politics",
+"futurology",
+"neoliberal",
+"wayofthebern",
+"worldpolitics",
+"socialism",
+"conspiracy",
+"conservative",
+"politicaldiscussion",
+"sandersforpresident",
+"anarcho_capitalism",
+"libertarian",
+"democrats",
+"republican",
+"politicalhumor",
+"atheism",
+"blackpeopletwitter",
+"latestagecapitalism",
+"political_revolution",
+"progressive",
+"nottheonion"]
+
+
+
 boringwords = ["the", "a", "she", "he", "is", "was", "should", "said", "people",
 			   "have", "her", "his", "an", "i", "in", "they", "for"
 			   ,"this", "you", "to", "are", "it", "that", "be", "with","on", "not", "but"
@@ -20,11 +46,8 @@ boringwords = ["the", "a", "she", "he", "is", "was", "should", "said", "people",
 			   "isn't", "things", "aren't", "enough", "doesn't", "without", "i’ll", "where", "whoever", "something",
 			   "think", "yet", "either" ]
 
-
 #Variables
-subreddit = "neoliberal"
 numberofcomments = 100
-
 
 def RepresentsInt(s):
     try:
@@ -32,13 +55,6 @@ def RepresentsInt(s):
         return True
     except ValueError:
         return False
-
-def transform_format(val):
-    if val == 0:
-        return 255
-    else:
-        return val
-
 
 # APIs
 api = twitter.Api(consumer_key='arDfIEpT8wrNZKtOUFBT6yXor',
@@ -51,42 +67,63 @@ reddit = praw.Reddit(client_id='RjprTHBXrucx7Q',
 					 user_agent='searches reddit(by u/derpoftheages)')
 
 
-comments = []
-i = 0
-time_probed = time.strftime("%H:%M:%S %Z")
-for comment in reddit.subreddit(subreddit).stream.comments():
-	if i == numberofcomments:
-		break
-	comments.append(comment)
-	i+=1
+class Thread(threading.Thread):
+	def __init__(self, subreddit):
+		threading.Thread.__init__(self)
+		self.subreddit = subreddit
 
-freq = {}
-remove_char = ['?', ',', '.', ':', '*', '(', ')', '!', '"', '[', ']', '#', '~', '-', '$']
+	def run(self):
+		getCommentsAndPostWordCloud(self.subreddit)
 
-for j in range(len(comments)):
-	comm3nt = comments[j].body.split()
-	for k in (range(len(comm3nt))):
-		current_word = comm3nt[k].lower()
-		current_word = ''.join([x for x in current_word if x not in remove_char])
-		if current_word not in boringwords and "http" not in current_word and not RepresentsInt(current_word):
-			if current_word not in freq:
-				freq[current_word] = 1
-			else:
-				freq[current_word] += 1
+def getCommentsAndPostWordCloud(subreddit):
+	comments = []
+	i = 0
+	time_probed = time.strftime("%H:%M:%S %Z")
+	for comment in reddit.subreddit(subreddit).stream.comments():
+		if i == numberofcomments:
+			break
+		comments.append(comment)
+		i+=1
 
-words = list(freq.keys())
-w = sorted(words,key= lambda x: freq[x], reverse=True)
-freqwords = ""
-for i in range(len(words)):
-	freqwords += (words[i] + ", ") * freq[words[i]]
+	freq = {}
+	remove_char = ['?', ',', '.', ':', '*', '(', ')', '!', '"', '[', ']', '#', '~', '-', '$']
 
-wordcloud = WordCloud(width=800, height=400, collocations=False, max_font_size=100, max_words=50, background_color="black").generate(freqwords)
-plt.figure(figsize=(20,10), facecolor='k')
-plt.imshow(wordcloud)
-plt.axis("off")
-plt.tight_layout(pad=0)
-plt.savefig('wordcloud.png')
+	for j in range(len(comments)):
+		comm3nt = comments[j].body.split()
+		for k in (range(len(comm3nt))):
+			current_word = comm3nt[k].lower()
+			current_word = ''.join([x for x in current_word if x not in remove_char])
+			if current_word not in boringwords and "http" not in current_word and not RepresentsInt(current_word):
+				if current_word not in freq:
+					freq[current_word] = 1
+				else:
+					freq[current_word] += 1
 
-api.PostUpdate("Here is a word cloud from " + "r/" + subreddit +" using the 'interesting' words from a " + str(numberofcomments) + " of the "
-				   "most recent comments posted to the sub. These comments were found at " + time_probed + ".",
-			   media= "wordcloud.png")
+	words = list(freq.keys())
+	freqwords = ""
+	for i in range(len(words)):
+		freqwords += (words[i] + ", ") * freq[words[i]]
+
+	wordcloud = WordCloud(width=800, height=400, collocations=False, max_font_size=100, max_words=50, background_color="black").generate(freqwords)
+	plt.figure(figsize=(20,10), facecolor='k')
+	plt.imshow(wordcloud)
+	plt.axis("off")
+	plt.tight_layout(pad=0)
+	plt.savefig('wordcloud.png')
+
+	api.PostUpdate("Here is a word cloud from www.reddit.com/r/" + subreddit +" using the 'interesting' words from a " + str(numberofcomments) + " of the "
+					"most recent comments posted to the sub. These comments were found at " + time_probed + ".",
+				   media= "wordcloud.png")
+
+#Main
+
+#select five random indices
+random_indices = random.sample(range(0, 21), 5)
+p = 0
+t = time.time()
+for i in random_indices:
+	selected_sub = subreddits[i]
+	getCommentsAndPostWordCloud(selected_sub)
+
+
+
